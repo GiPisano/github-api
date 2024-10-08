1f90c82814009d162cff37bf6e9f973203eae701
GiPisano <pisanogiacomo9@gmail.com>
2024-10-09 00:03:11 +0200

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ const query = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 const results = document.getElementById("results");
 
-// EventListener for search btn
+// EventListener for the search btn
 searchBtn.addEventListener("click", function () {
   const query = searchInput.value.trim();
 
   if (query.length === 0) {
-    alert("Inserisci un nome per cercare i repository.");
+    alert("Please enter a name to search for repositories.");
     return;
   }
 
   searchRepositories(query);
 });
 
-// Function "search the repositoies" (input)
+// Function to search for repositories based on the input query
 function searchRepositories(query) {
   const url = `https://api.github.com/search/repositories?q=${query}`;
 
@@ -25,8 +25,7 @@ function searchRepositories(query) {
     })
     .catch((error) => {
       console.error("Errore:", error);
-      results.innerHTML =
-        "<p>Si è verificato un errore durante la ricerca.</p>";
+      results.innerHTML = "<p>An error occurred during the search.</p>";
     });
 }
 
